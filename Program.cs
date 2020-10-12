aa7442352c1b5c94d21899844440a3844b0ba9af
Sam Carson <carsonsam4@gmail.com>
2020-10-12 12:32:44 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+using System.Text.RegularExpressions;
+
+namespace Stack_Queue_Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rawUserInput;
+            string userInput;
+            string repeatProgram;
+
+
+            Write("Give me a palindrome! --> ");
+            rawUserInput = ReadLine().ToUpper();
+
+            do
+            {
+                Regex regularExpression = new Regex("[^A-Z0-9]");
+                userInput = regularExpression.Replace(rawUserInput, "");
+
+                char[] array = userInput.ToCharArray();
+
+                Write("Give me another palindrome! (say 'no' to exit) --> ");
+                repeatProgram = ReadLine().ToUpper();
+                if (repeatProgram != "NO")
+                {
+                    rawUserInput = repeatProgram;
+                }
+
+            } while (repeatProgram != "NO");
+
+            WriteLine("Later gator!");
+
+        }
+    }
+}
---
+++ StackArray.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack_Queue_Palindrome
+{
+    public class StackArray
+    {
+    }
+}
